e9727eafd2b492a5410f2e188bda306f3297830b
RafliRasyidin <raflirasyidin20@gmail.com>
2021-03-28 23:18:00 +0700

    Create base adapter

---
+++ app/src/main/java/com/rasyidin/githubapp/core/adapter/BaseAdapter.kt
@@ -0,0 +1,33 @@
+package com.rasyidin.githubapp.core.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseAdapter<T>(private val layoutId: Int) :
+    RecyclerView.Adapter<BaseAdapter.BaseViewHolder>() {
+
+    protected var data = ArrayList<T>()
+
+    fun setData(listItem: List<T>?) {
+        if (listItem.isNullOrEmpty()) return
+        data.clear()
+        data.addAll(listItem)
+        notifyDataSetChanged()
+    }
+
+    var onItemClickListener: ((T) -> Unit)? = null
+
+    class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return BaseViewHolder(
+            LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
--- app/src/main/java/com/rasyidin/githubapp/core/adapter/UserAdapter.kt
+++ app/src/main/java/com/rasyidin/githubapp/core/adapter/UserAdapter.kt
@@ -1,69 +1,30 @@
 package com.rasyidin.githubapp.core.adapter
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.rasyidin.githubapp.R
 import com.rasyidin.githubapp.core.domain.model.User
 import com.rasyidin.githubapp.databinding.ItemListUserBinding
 
-class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+class UserAdapter : BaseAdapter<User>(R.layout.item_list_user) {
 
-    private var users = ArrayList<User>()
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        val user = data[position]
+        val binding = ItemListUserBinding.bind(holder.itemView)
+        with(binding) {
+            Glide.with(holder.itemView)
+                .load(user.avatar)
+                .placeholder(R.drawable.ic_github)
+                .error(R.drawable.ic_broken_image)
+                .into(imgUser)
 
-    fun setData(user: List<User>) {
-        if (user.isNullOrEmpty()) return
-        users.clear()
-        users.addAll(user)
-        notifyDataSetChanged()
-    }
+            tvUsername.text = user.username
+            tvType.text = user.name
 
-    var onItemClick: ((User) -> Unit)? = null
-
-    inner class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val binding = ItemListUserBinding.bind(itemView)
-        fun bind(user: User) {
-            with(binding) {
-                Glide.with(itemView.context)
-                    .load(
-                        itemView.context.resources.getIdentifier(
-                            user.avatar,
-                            "drawable",
-                            itemView.context.packageName
-                        )
-                    )
-                    .into(imgUser)
-
-                tvUsername.text = user.username
-                tvName.text = user.name
-                tvCompany.text = user.company
-
+            root.setOnClickListener {
+                onItemClickListener?.invoke(user)
             }
+
         }
 
-        init {
-            binding.root.setOnClickListener {
-                onItemClick?.invoke(users[adapterPosition])
-            }
-        }
-
-    }
-
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        return UserViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_list_user, parent, false)
-        )
-    }
-
-    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        val data = users[position]
-        holder.bind(data)
-    }
-
-    override fun getItemCount(): Int {
-        return users.size
     }
 }
