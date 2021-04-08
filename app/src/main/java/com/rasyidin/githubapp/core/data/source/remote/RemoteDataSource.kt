027a25358a80af9341fcaeb40a1b4cc57a993379
RafliRasyidin <raflirasyidin20@gmail.com>
2021-04-08 21:23:07 +0700

    change to flow.catch

--- app/src/main/java/com/rasyidin/githubapp/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/rasyidin/githubapp/core/data/source/remote/RemoteDataSource.kt
@@ -6,6 +6,7 @@ import com.rasyidin.githubapp.core.data.source.remote.network.ApiService
 import com.rasyidin.githubapp.core.data.source.remote.response.UserItemResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
@@ -17,82 +18,72 @@ class RemoteDataSource(private val apiService: ApiService) {
 
     suspend fun getUsers(): Flow<ApiResponse<List<UserItemResponse>>> {
         return flow {
-            try {
-                val response = apiService.getUsers()
-                if (response.isNotEmpty()) {
-                    emit(ApiResponse.Success(response))
-                } else {
-                    emit(ApiResponse.Empty)
-                }
-            } catch (e: Exception) {
-                emit(ApiResponse.Error(e.message.toString()))
-                Log.e("RemoteDataSource", e.message.toString())
+            val response = apiService.getUsers()
+            if (response.isNotEmpty()) {
+                emit(ApiResponse.Success(response))
+            } else {
+                emit(ApiResponse.Empty)
             }
+        }.catch { e ->
+            emit(ApiResponse.Error(e.message.toString()))
+            Log.e("RemoteDataSource", e.message.toString())
         }.flowOn(Dispatchers.IO)
     }
 
     suspend fun getDetailUser(username: String?): Flow<ApiResponse<UserItemResponse>> {
         return flow {
-            try {
-                val response = apiService.getDetailUser(username)
-                if (response.username.isNotEmpty()) {
-                    emit(ApiResponse.Success(response))
-                } else {
-                    emit(ApiResponse.Empty)
-                }
-            } catch (e: Exception) {
-                emit(ApiResponse.Error(e.message.toString()))
-                Log.e(TAG, e.message.toString())
+            val response = apiService.getDetailUser(username)
+            if (response.username.isNotEmpty()) {
+                emit(ApiResponse.Success(response))
+            } else {
+                emit(ApiResponse.Empty)
             }
-        }
+        }.catch { e ->
+            emit(ApiResponse.Error(e.message.toString()))
+            Log.e(TAG, e.message.toString())
+        }.flowOn(Dispatchers.IO)
     }
 
     suspend fun getUserFollowers(username: String?): Flow<ApiResponse<List<UserItemResponse>>> {
         return flow {
-            try {
-                val response = apiService.getUserFollowers(username)
-                if (response.isNotEmpty()) {
-                    emit(ApiResponse.Success(response))
-                } else {
-                    emit(ApiResponse.Empty)
-                }
-            } catch (e: Exception) {
-                emit(ApiResponse.Error(e.message.toString()))
-                Log.e(TAG, e.message.toString())
+            val response = apiService.getUserFollowers(username)
+            if (response.isNotEmpty()) {
+                emit(ApiResponse.Success(response))
+            } else {
+                emit(ApiResponse.Empty)
             }
+        }.catch { e ->
+            emit(ApiResponse.Error(e.message.toString()))
+            Log.e(TAG, e.message.toString())
         }.flowOn(Dispatchers.IO)
     }
 
     suspend fun getUserFollowing(username: String?): Flow<ApiResponse<List<UserItemResponse>>> {
         return flow {
-            try {
-                val response = apiService.getUserFollowing(username)
-                if (response.isNotEmpty()) {
-                    emit(ApiResponse.Success(response))
-                } else {
-                    emit(ApiResponse.Empty)
-                }
-            } catch (e: Exception) {
-                emit(ApiResponse.Error(e.message.toString()))
-                Log.e(TAG, e.message.toString())
+            val response = apiService.getUserFollowing(username)
+            if (response.isNotEmpty()) {
+                emit(ApiResponse.Success(response))
+            } else {
+                emit(ApiResponse.Empty)
             }
+        }.catch { e ->
+            emit(ApiResponse.Error(e.message.toString()))
+            Log.e(TAG, e.message.toString())
         }.flowOn(Dispatchers.IO)
     }
 
     suspend fun searchUsers(query: String?): Flow<ApiResponse<List<UserItemResponse>>> {
         return flow {
-            try {
-                val response = apiService.searchUsers(query)
-                val data = response.items
-                if (data.isNotEmpty()) {
-                    emit(ApiResponse.Success(data))
-                } else {
-                    emit(ApiResponse.Empty)
-                }
-            } catch (e: Exception) {
-                emit(ApiResponse.Error(e.message.toString()))
-                Log.e(TAG, e.message.toString())
+            val response = apiService.searchUsers(query)
+            val data = response.items
+            if (data.isNotEmpty()) {
+                emit(ApiResponse.Success(data))
+            } else {
+                emit(ApiResponse.Empty)
             }
+        }.catch { e ->
+            emit(ApiResponse.Error(e.message.toString()))
+            Log.e(TAG, e.message.toString())
         }.flowOn(Dispatchers.IO)
     }
 }
