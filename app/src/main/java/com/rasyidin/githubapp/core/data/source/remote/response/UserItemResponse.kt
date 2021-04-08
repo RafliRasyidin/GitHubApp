241345fcc25a0151bd9270252b57cf2dfc1731c4
RafliRasyidin <raflirasyidin20@gmail.com>
2021-04-08 21:22:12 +0700

    change some properties to nullable

--- app/src/main/java/com/rasyidin/githubapp/core/data/source/remote/response/UserItemResponse.kt
+++ app/src/main/java/com/rasyidin/githubapp/core/data/source/remote/response/UserItemResponse.kt
@@ -5,32 +5,32 @@ import com.google.gson.annotations.SerializedName
 data class UserItemResponse(
 
     @field:SerializedName("id")
-    val id: Int,
+    val id: Int = 0,
 
     @field:SerializedName("avatar_url")
-    val avatarUrl: String,
+    val avatarUrl: String? = "",
 
     @field:SerializedName("company")
-    val company: String,
+    val company: String? = "",
 
     @field:SerializedName("followers")
-    val follower: Int,
+    val follower: Int = 0,
 
     @field:SerializedName("following")
-    val following: Int,
+    val following: Int = 0,
 
     @field:SerializedName("location")
-    val location: String,
+    val location: String? = "",
 
     @field:SerializedName("name")
-    val name: String,
+    val name: String? = "",
 
     @field:SerializedName("public_repos")
-    val repository: Int,
+    val repository: Int = 0,
 
     @field:SerializedName("login")
     val username: String,
 
     @field:SerializedName("type")
-    val type: String
+    val type: String? = ""
 )
