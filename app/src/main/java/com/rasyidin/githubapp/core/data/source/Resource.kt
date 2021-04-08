06a83496faf217a805bcdfafa88d07c7626e5871
RafliRasyidin <raflirasyidin20@gmail.com>
2021-04-08 21:23:58 +0700

    initialize Loading(data) to null

--- app/src/main/java/com/rasyidin/githubapp/core/data/source/Resource.kt
+++
@@ -1,8 +0,0 @@
-package com.rasyidin.githubapp.core.data.source
-
-sealed class Resource<T>(val data: T?, val message: String? = null) {
-    class Success<T>(data: T?) : Resource<T>(data)
-    class Loading<T>(data: T?): Resource<T>(data)
-    class Error<T>(data: T? = null, message: String?): Resource<T>(data, message)
-
-}
