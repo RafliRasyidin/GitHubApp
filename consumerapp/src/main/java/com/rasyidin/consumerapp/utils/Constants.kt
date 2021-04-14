fe458bc29184fefe9d7fb8bb2d3d830e66a4deb4
RafliRasyidin <raflirasyidin20@gmail.com>
2021-04-15 00:53:00 +0700

    remove unused resources

--- consumerapp/src/main/java/com/rasyidin/consumerapp/utils/Constants.kt
+++ consumerapp/src/main/java/com/rasyidin/consumerapp/utils/Constants.kt
@@ -2,14 +2,6 @@ package com.rasyidin.consumerapp.utils
 
 object Constants {
 
-    const val SPLASH_DELAY = 2000L
-
-    const val BASE_URL = "https://api.github.com/"
-
-    const val REQUEST_TIMEOUT = 120L
-
-    const val DEBOUNCE_DURATION = 500L
-
     const val SUFFIX_CHARS = "KMBTPE"
 
     const val TABLE_FAVORITE = "favorite"
--- consumerapp/src/main/java/com/rasyidin/consumerapp/utils/Helper.kt
+++ consumerapp/src/main/java/com/rasyidin/consumerapp/utils/Helper.kt
@@ -4,11 +4,6 @@ import android.database.Cursor
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.rasyidin.consumerapp.model.User
-import com.rasyidin.consumerapp.utils.Constants.SUFFIX_CHARS
-import java.math.RoundingMode
-import java.text.DecimalFormat
-import kotlin.math.ln
-import kotlin.math.pow
 
 fun ImageView.loadImage(url: String?, placeHolder: Int, errorImage: Int) {
     Glide.with(this.context)
@@ -17,20 +12,6 @@ fun ImageView.loadImage(url: String?, placeHolder: Int, errorImage: Int) {
         .error(errorImage)
         .centerCrop()
         .into(this)
-}
-
-fun Int.toShortNumberDisplay(): String {
-    val doubleNumber = this.toDouble()
-    val formatter = DecimalFormat("###.#")
-    val isNumberLessThanThousand = this < 1000.0
-    formatter.roundingMode = RoundingMode.DOWN
-
-    return if (isNumberLessThanThousand) {
-        formatter.format(doubleNumber)
-    } else {
-        val exp = (ln(doubleNumber) / ln(1000.0)).toInt()
-        formatter.format(this / 1000.0.pow(exp.toDouble())) + SUFFIX_CHARS[exp - 1]
-    }
 }
 
 fun cursorToArrayList(cursor: Cursor?): ArrayList<User> {
